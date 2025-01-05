46d853939dbdcaf456c49e8ecb7ecf09793584fc
adrian rs <adrian.r.strate@gmail.com>
2025-01-05 14:32:10 +0100

    (master) MOAR examples copied. Also exit-game plugin

---
+++ example-copies/4-scene3d/src/main.rs
@@ -0,0 +1,41 @@
+use bevy::prelude::*;
+use exit_game::ExitGamePlugin;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(ExitGamePlugin)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Circle::new(4.0))),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+    ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(120, 0, 180))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+    ));
+
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+}
---
+++ plugins/exit-game/src/lib.rs
@@ -0,0 +1,56 @@
+use bevy::{
+    app::AppExit,
+    input::{gamepad::GamepadEvent, keyboard::KeyboardInput},
+    prelude::*,
+};
+
+pub struct ExitGamePlugin;
+
+impl Plugin for ExitGamePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<ExitGameEvent>()
+            .add_systems(Update, (keyboard_input, gamepad_input))
+            .add_systems(Last, exit_game);
+    }
+}
+
+#[derive(Event)]
+struct ExitGameEvent;
+
+fn exit_game(ev: EventReader<ExitGameEvent>, mut ev_exit: EventWriter<AppExit>) {
+    if !ev.is_empty() {
+        ev_exit.send(AppExit::Success);
+    }
+}
+
+fn keyboard_input(
+    mut input_events: EventReader<KeyboardInput>,
+    mut ev: EventWriter<ExitGameEvent>,
+) {
+    for input in input_events.read() {
+        if input.state.is_pressed() {
+            match &input.key_code {
+                KeyCode::Escape => {
+                    ev.send(ExitGameEvent);
+                }
+                _ => continue,
+            }
+        }
+    }
+}
+
+fn gamepad_input(
+    mut gamepad_events: EventReader<GamepadEvent>,
+    mut ev: EventWriter<ExitGameEvent>,
+) {
+    for event in gamepad_events.read() {
+        match event {
+            GamepadEvent::Button(button) => {
+                if button.button == GamepadButton::Select && button.state.is_pressed() {
+                    ev.send(ExitGameEvent);
+                }
+            }
+            _ => continue,
+        }
+    }
+}
